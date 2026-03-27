8d7ee3f4eaea4d9e4bb3b143609942efe008c36b
yashdhande26 <dhandeyash94@gmail.com>
2026-03-27 16:35:07 +0530

    Add photo

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
             <div className="hero-avatar-wrapper">
               <div className="hero-avatar-inner">
                 <img 
-                  src="https://media.licdn.com/dms/image/v2/D5635AQHnp-guVuL85g/profile-framedphoto-shrink_200_200/B56Z0u68deHkAc-/0/1774608697433?e=1775214000&v=beta&t=jgDPUe7VJCapEAVTGLRH7rc4STm6rSVZToxG9VHi_Oc"
+                  src="/profile.png"
                   alt="Profile"
                   className="hero-avatar-img"
                 />
